f50868fe1c5dd10904bc7b8bad881b66b68d2c32
simsime12 <jove3120@gmail.com>
2023-04-19 20:16:50 +0900

    mobile

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React234
-        </a>
-      </header>
+      <div className="container">
+        <div className='product'>
+          <div className='pBox'></div>
+          <div className='pBox'></div>
+          <div className='pBox'></div>
+        </div>
+      </div>
     </div>
   );
 }
