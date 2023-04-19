b2f9dc13c02c5cdd5e5483c28f1db5a1be68d759
simsime12 <jove3120@gmail.com>
2023-04-19 19:49:44 +0900

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React234
         </a>
       </header>
     </div>
